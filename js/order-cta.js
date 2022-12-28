65c6dde58165ef6926318595911e784dae0976a5
waterbinnn <ewaterbin@naver.com>
2022-12-28 18:34:21 +0900

    ♻️ Handle orderform modal

---
+++ js/order-cta.js
@@ -0,0 +1,18 @@
+const orderCta = document.querySelector('.order-cta')
+const [orderCtaBookmarkBtn, orderCtaBuyBtn] = orderCta.children
+
+const orderModal = document.querySelector('.order-form-modal')
+const orderModalOverlay = document.querySelector('.overlay')
+
+function openOrderModal() {
+  orderModal.classList.add('is-open')
+  orderModalOverlay.classList.add('is-active')
+}
+
+function closeOrderModal() {
+  orderModal.classList.remove('is-open')
+  orderModalOverlay.classList.remove('is-active')
+}
+
+orderCtaBuyBtn.addEventListener('click', openOrderModal)
+orderModalOverlay.addEventListener('click', closeOrderModal)
