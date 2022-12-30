bd4345f92b39710864652e21c6201beaa0913b3b
waterbinnn <ewaterbin@naver.com>
2022-12-30 23:02:01 +0900

    🚧 변수선언

---
+++ js/order-form.js
@@ -0,0 +1,9 @@
+const orderForm = document.querySelector('.floating-order-form')
+const bookmarkButton = orderForm.querySelector('.bookmark-button')
+
+const cartButton = orderForm.querySelector('.button-group .btn-outlined')
+const cartModal = document.querySelector('.cart-modal')
+const cartModalOverlay = document.querySelector('.overlay')
+const cartCloseButton = cartModal.querySelector('.btn-secondary')
+
+const globalFooter = document.querySelector('.global-footer')
